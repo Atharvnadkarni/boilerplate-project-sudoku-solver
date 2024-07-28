63a964b6466b2cbb1e61158a39009ed7613a231b
AtharvNadkarni2013 <81621785+AtharvNadkarni2013@users.noreply.github.com>
2024-07-28 11:33:23 +0530

    comm

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -1,6 +1,8 @@
 class SudokuSolver {
 
   validate(puzzleString) {
+    if (!puzzleString.length == 81) throw Error
+    if (puzzleString.match(/[^1-9]/g) && puzzleString.match(/[^.]/g)) throw Error
   }
 
   checkRowPlacement(puzzleString, row, column, value) {
